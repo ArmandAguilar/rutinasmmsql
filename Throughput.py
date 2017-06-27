8ed29757db1294fb50c4acaea2e3809586b03a6f
Armando Aguilar L <a.aguilar@gategeek.com>
2017-06-27 14:03:54 -0500

    Test the skull

---
+++ Throughput.py
@@ -0,0 +1,44 @@
+#!user/bin/python
+from tokens import *
+import unicodedata
+import pypyodbc as pyodbc
+import pymssql
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+# 1 .- Step get a list of proyect of  to insert
+# 2 .- Search the if proyect exit in the table
+#   2.1 if Proyecto exist update data
+#   2.2 if proyect don´t exist insert data
+
+def fieldExist(NumProyect,NumMaster):
+    Accion = 'No'
+    sql_buscar = 'SELECT [Id] FROM [SAP].[dbo].[AAAThroughput] Where [NumProyecto] = \'' + str(NumProyect) + '\' and [NumMatestro] = \'' + str(NumMaster) + '\''
+    conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
+    cur = conn.cursor()
+    cur.execute(sql_buscar)
+    for value in cur:
+         Accion = 'Si'
+    conn.commit()
+    conn.close()
+
+    return Accion
+
+def insertTroughtPut(sql):
+    conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+
+def upadteTroughtPut(sql):
+    conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+
+print('###################################### Begin Calculando Throughput ######################################')
+
+
+print('###################################### End Calculando Throughput ######################################')
