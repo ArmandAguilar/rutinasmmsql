cbaf5a7feea236e7a38173a57738ca4cf59f3164
Armando Aguilar L <a.aguilar@gategeek.com>
2017-06-28 16:11:34 -0500

    28/06/2017 4:57

--- Throughput.py
+++ Throughput.py
@@ -1,7 +1,6 @@
 #!user/bin/python
 from tokens import *
 import unicodedata
-import pypyodbc as pyodbc
 import pymssql
 
 
@@ -52,7 +51,7 @@ for value in cur:
     Trabajoporprogramar = value[4]
     MargenActual = value[5]
     PeriodoComparativo = value[6]
-    mAction = fieldExist(value[0],value[2])
+    #mAction = fieldExist(value[0],value[2])
     if mAction == 'Si':
         #update
         Sql = 'update'
