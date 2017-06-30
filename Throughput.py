ad29ae835d1e81f93740b9cd0098dad0c5df71fd
Armando Aguilar L <a.aguilar@gategeek.com>
2017-06-30 11:20:02 -0500

    Script Fri 11:20 am

--- Throughput.py
+++ Throughput.py
@@ -38,7 +38,7 @@ def upadteTroughtPut(sql):
 
 print('######################################## egin Calculando Throughput #######################################')
 
-sql = 'SELECT [NumProyecto],[NomProyecto],[NumMaestro],[Dias de produccion],[Trabajo por programar],[PeriodoComparativo],ISNULL([Margen Actual],0) AS MargenActual FROM [SAP].[dbo].[RV-ESTADOPROYECTOS-AA-Throughput]'
+sql = 'SELECT [NumProyecto],[NomProyecto],[NumMaestro],[Dias de produccion],[Trabajo por programar],[PeriodoComparativo],Convert(VARCHAR(100),[Margen Actual]) AS MargenActual FROM [SAP].[dbo].[RV-ESTADOPROYECTOS-AA-Throughput]'
 conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=dbMSSQL)
 cur = conn.cursor()
 cur.execute(sql)
