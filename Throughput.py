c74375c3d4b3834db7a6fc92ae510f799953364b
Armando Aguilar L <a.aguilar@gategeek.com>
2017-07-06 22:18:30 -0500

    Thu  06  10:18 pm

--- Throughput.py
+++ Throughput.py
@@ -84,7 +84,7 @@ data = json.dumps(ListDataJson)
 dataJson = json.loads(data)
 #print(dataJson)
 for value in dataJson[1]:
-    print (str(value[Id]))
+    print (str(value['Id']))
 #sql = 'SELECT [NumProyecto],[NumMaestro],[Dias de produccion],[Trabajo por programar],[Margen Actual],[PeriodoComparativo] FROM [SAP].[dbo].[RV-ESTADOPROYECTOS-AA-Throughput]'
 #con = pyodbc.connect(constr)
 #cur = con.cursor()
