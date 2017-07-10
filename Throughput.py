6c0858d7e5c1a3cb12746cb1d04aa0f713029729
Armando Aguilar L <a.aguilar@gategeek.com>
2017-07-10 09:51:59 -0500

    Mon 10 09:51am

--- Throughput.py
+++ Throughput.py
@@ -87,6 +87,7 @@ print('######################################### Begin Calculando Throughput ###
 
 #2 .- We read the list and create the sql for calulate te thoriughput
 # 2 .- Read the list for years
+ListDataJson = '{"employees":['
 for valueYear in listYears:
     sql = 'SELECT [NumProyecto],[NumMaestro],[Dias de produccion] As DiasDeProduccion ,[Trabajo por programar] As TrabajoPorProgramar,[Margen Actual] As MargenActual,[PeriodoComparativo] FROM [SAP].[dbo].[RV-ESTADOPROYECTOS-AA-Throughput] where [PeriodoComparativo] =\'' + str(valueYear) + '\' order by NumMaestro'
     print (sql)
@@ -101,7 +102,7 @@ ListDataJson += ']}'
 
 dataJson = json.loads(ListDataJson)
 
-print dataJson 
+print dataJson
 #for value in dataJson['fields']:
 #      print value['Id']
 
