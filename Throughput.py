4f6f1827246cf2a1109d4442a4ac88c631ada191
Armando Aguilar L <a.aguilar@gategeek.com>
2017-07-10 10:58:29 -0500

    Mon 10 10:58am

--- Throughput.py
+++ Throughput.py
@@ -96,8 +96,10 @@ for valueYear in listYears:
     cur.execute(sql)
     for value in cur:
         #passMSSQL
-        ListDataJson += '{"Id" : "' + str(DNI) + '","NumProyecto" : "' + str(value[0]) + '","NumMaestro" : "' + str(value[1]) + '","DiasDeProduccion" : "' + str(value[2]) + '","TrabajoPorProgramar" : "' + str(value[3]) + '","MargenActual" : "' + str(value[4]) + '","PeriodoComparativo" : "' + str(value[5]) + '"},'
-        DNI += 1
+        if valueYear == 2016:
+            #pass
+            ListDataJson += '{"Id" : "' + str(DNI) + '","NumProyecto" : "' + str(value[0]) + '","NumMaestro" : "' + str(value[1]) + '","DiasDeProduccion" : "' + str(value[2]) + '","TrabajoPorProgramar" : "' + str(value[3]) + '","MargenActual" : "' + str(value[4]) + '","PeriodoComparativo" : "' + str(value[5]) + '"},'
+            DNI += 1
 ListDataJson += ']}'
 print ListDataJson
 
