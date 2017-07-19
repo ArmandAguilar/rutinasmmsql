42ee9dd64c0b3c6fe51ae6f9130fb4cde0c1dda0
Armando Aguilar L <a.aguilar@gategeek.com>
2017-07-19 12:02:59 -0500

    Wed 19  12:02 pm

--- Throughput.py
+++ Throughput.py
@@ -150,7 +150,7 @@ listCompanysA = list(set(listCompanys))
 
 
 #Here we create us a new Json with all data of  (RV-ESTADOPROYECTOS-AA-Throughput)  with this data we go a new json for slice and comparte companys
-sql = 'SELECT [NumMaestro],[Dias de produccion] As DiasDeProduccion ,[Trabajo por programar] As TrabajoPorProgramar,[Margen Actual] As MargenActual, ISNULL([PeriodoComparativo],1999) As PeriodoComparativo,[Empresa]  FROM [SAP].[dbo].[RV-ESTADOPROYECTOS-AA-Throughput] order by NumMaestro'
+sql = 'SELECT [NumMaestro],[Dias de produccion] As DiasDeProduccion ,[Trabajo por programar] As TrabajoPorProgramar,[Margen Actual] As MargenActual, ISNULL([PeriodoComparativo],1999) As PeriodoComparativo,[Empresa],[NumProyecto]  FROM [SAP].[dbo].[RV-ESTADOPROYECTOS-AA-Throughput] order by NumMaestro'
 con = pyodbc.connect(constr)
 cur = con.cursor()
 cur.execute(sql)
@@ -159,7 +159,7 @@ ListDataJsonCompanys = '{"Companys":['
 DNI = 0
 for value in cur:
     if value[0] > 0:
-        ListDataJsonCompanys += '{"NumMaestro":' + str(value[0]) + ',"DiasDeProduccion": ' + str(value[1]) + ',"TrabajoPorProgramar":' + str(value[2]) + ',"MargenActual" : ' + str(value[3]) + ',"PeriodoComparativo":' + str(value[4]) + ',"Empresa":"' + str(value[5]) + '"},' + '\n'
+        ListDataJsonCompanys += '{"NumProyecto : "' +  str(value[5])  + ',"NumMaestro":' + str(value[0]) + ',"DiasDeProduccion": ' + str(value[1]) + ',"TrabajoPorProgramar":' + str(value[2]) + ',"MargenActual" : ' + str(value[3]) + ',"PeriodoComparativo":' + str(value[4]) + ',"Empresa":"' + str(value[5]) + '"},' + '\n'
 
 con.commit()
 con.close()
@@ -179,7 +179,7 @@ for value in dataCompanys['Companys']:
     if value['Empresa'] == 'Quaker State':
         if value['PeriodoComparativo'] == 2017:
             if value['NumMaestro'] == 963:
-                print( "NumMaestro : " + str(value['NumMaestro']) + "Empresa :" + str(value['Empresa']) + 'Margen Actual:$' +  str(value['MargenActual']) + 'Periodo Comparativo :' + str(value['PeriodoComparativo']))
+                print( ' NumProyecto' + str(value['NumProyecto']) + 'NumMaestro : ' + str(value['NumMaestro']) + 'Empresa :' + str(value['Empresa']) + 'Margen Actual:$' +  str(value['MargenActual']) + 'Periodo Comparativo :' + str(value['PeriodoComparativo']))
                 MargenXMaestroEmpresa += value['MargenActual']
                 DiasDeProduccion += value['DiasDeProduccion']
                 TrabajoPorProgramar += value['TrabajoPorProgramar']
