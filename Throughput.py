d7444a9ccaa3567e916c29fc3e8e19dea14ad557
Armando Aguilar L <a.aguilar@gategeek.com>
2017-07-21 12:22:15 -0500

    thu 20 12:22 am

--- Throughput.py
+++ Throughput.py
@@ -47,7 +47,7 @@ def ThroughputClients(Empresa,Periodo,NumMaestro,ListDataJsonCompanys):
         if value['Empresa'] == str(Empresa):
             if value['PeriodoComparativo'] == Periodo:
                 if int(value['NumMaestro']) == int(NumMaestro):
-                    #print('#####Armando#####NumProyecto' + str(value['NumProyecto']) + 'NumMaestro : ' + str(value['NumMaestro']) + 'Empresa :' + str(value['Empresa']) + 'Margen Actual:$' +  str(value['MargenActual']) + 'Periodo Comparativo :' + str(value['PeriodoComparativo']))
+                    print('#####Armando#####NumProyecto' + str(value['NumProyecto']) + 'NumMaestro : ' + str(value['NumMaestro']) + 'Empresa :' + str(value['Empresa']) + 'Margen Actual:$' +  str(value['MargenActual']) + 'Periodo Comparativo :' + str(value['PeriodoComparativo']))
                     MargenXMaestroEmpresa += value['MargenActual']
                     DiasDeProduccion += value['DiasDeProduccion']
                     TrabajoPorProgramar += value['TrabajoPorProgramar']
@@ -95,8 +95,8 @@ def tDataJason(ListDataJson,periodo,listMaestrosA,ListMargenJson,ListDataJsonCom
             TrhoughputR = MargenXMaestro/x
         else:
             TrhoughputR = 0
-        ThroughputRC = ThroughputClients(valueJson['Empresa'],periodo,valueJson['NumMaestro'],ListDataJsonCompanys)
-        print ('NoMaestro :' + str(valueListMaestros)  + ' Throughput Maestro : $' + str(TrhoughputR) + ' Throughput Maestro: $' + str(ThroughputRC))
+        ThroughputRClients = ThroughputClients(valueJson['Empresa'],periodo,valueJson['NumMaestro'],ListDataJsonCompanys)
+        print ('NoMaestro :' + str(valueListMaestros)  + ' Throughput Maestro : $' + str(TrhoughputR) + ' Throughput Maestro: $' + str(ThroughputRClients))
         #print('############Empresa:' + str(valueJson['Empresa']) + ' NumMaestro: ' + valueJson['NumMaestro'] + ' Periodo:' + str(periodo))
     #print ListDataJson
     #print listMaestrosA
