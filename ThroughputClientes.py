23e6689b8c1b1ec327761403dadde78698aa61b3
Armando Aguilar L <a.aguilar@gategeek.com>
2017-07-27 15:35:05 -0500

    Thu 27 03:35 pm

--- ThroughputClientes.py
+++ ThroughputClientes.py
@@ -103,5 +103,5 @@ for valuePeridos in listYears:
                 TrhoughputRC = MargenXMaestroEmpresa/x
             else:
                 TrhoughputRC = 0
-            print ('Periodo : ' + str(valuePeridos) + ' Empresa: ' + str(valueIdEmpresa) + 'Dias De Produccion: ' + str(DiasDeProduccion) + ' Trabajo Por Programar :' + str(TrabajoPorProgramar) + ' Margen Actual: $' + str(MargenXMaestroEmpresa) + 'TrhoughputCliente' + str(TrhoughputRC))
+            print ('Periodo : ' + str(valuePeridos) + ' Empresa: ' + str(valueIdEmpresa) + '-' + str(valueCom['Empresa']) + 'Dias De Produccion: ' + str(DiasDeProduccion) + ' Trabajo Por Programar :' + str(TrabajoPorProgramar) + ' Margen Actual: $' + str(MargenXMaestroEmpresa) + 'TrhoughputCliente' + str(TrhoughputRC))
 print('##################################### End Calculando Throughput ######################################')
